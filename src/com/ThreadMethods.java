d5673b4462ab2f4f13d5c55caf92fd783ae766f5
tanayvaswani <vaswani.tanay9@gmail.com>
2024-08-17 04:47:20 +0530

    feat: Thread Methods .interrupt()

--- src/com/ThreadMethods.java
+++ src/com/ThreadMethods.java
@@ -7,29 +7,36 @@ public class ThreadMethods extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(Thread.currentThread().getName() + " - Priority: " + Thread.currentThread().getPriority() + " - Count: " + i);
+        // For .setPriority()
+        // for (int i = 0; i < 5; i++) {
+            // System.out.println(Thread.currentThread().getName() + " - Priority: " + Thread.currentThread().getPriority() + " - Count: " + i);
 
             try {
                 Thread.sleep(500);
+                // For .interrupt()
+                System.out.println("Thread is running...");
             } catch (InterruptedException e) {
-                System.out.println(e);
+                System.out.println("Thread Interrupted: " + e);
             }
         }
-    }
+    // }
 
     public static void main(String[] args) {
-        ThreadMethods l = new ThreadMethods("Low");
-        ThreadMethods m = new ThreadMethods("Medium");
-        ThreadMethods h = new ThreadMethods("High");
+        // ThreadMethods l = new ThreadMethods("Low");
+        // ThreadMethods m = new ThreadMethods("Medium");
+        // ThreadMethods h = new ThreadMethods("High");
 
-        l.setPriority(Thread.MIN_PRIORITY);
-        m.setPriority(Thread.NORM_PRIORITY);
-        h.setPriority(Thread.MAX_PRIORITY);
+        // l.setPriority(Thread.MIN_PRIORITY);
+        // m.setPriority(Thread.NORM_PRIORITY);
+        // h.setPriority(Thread.MAX_PRIORITY);
 
-        l.start();
-        m.start();
-        h.start();
+        // l.start();
+        // m.start();
+        // h.start();
+
+        ThreadMethods tm = new ThreadMethods("Interrupt");
+        tm.start();
+        tm.interrupt();
     }
 }
 
@@ -39,3 +46,5 @@ public class ThreadMethods extends Thread {
 // - sleep()
 // - join()
 // - setPriority()
+// - interrupt()
+// - yield()
