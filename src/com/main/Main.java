02ae31503d0fb72b8a72f245a45fedb56403f402
tanayvaswani <vaswani.tanay9@gmail.com>
2024-08-16 19:38:51 +0530

    feat: 2nd approach

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,13 +1,20 @@
 package com.main;
 
+import com.newMain.NewMain;
 import com.submain.SubMain;
 
 public class Main {
     public static void main(String[] args) {
+        // Approach1 : by implementing Runnable Class
         SubMain sm = new SubMain();
         Thread nt1 = new Thread(sm);
         nt1.start();
 
+        // Approach 2 : by extending Thread Class
+        NewMain nm = new NewMain();
+        nm.start();
+
+        // thread for main
         for (int i = 0; i < 100; i++) {
             System.out.println(Thread.currentThread().getName());
         }
