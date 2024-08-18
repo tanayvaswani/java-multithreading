6a46aa5f03a095fe2c566b3101612aaf8c87fb31
tanayvaswani <vaswani.tanay9@gmail.com>
2024-08-19 01:29:26 +0530

    feat: synchronized block in method

--- src/com/sync/Counter.java
+++ src/com/sync/Counter.java
@@ -4,8 +4,11 @@ public class Counter {
 
     private int count = 0;
 
-    public synchronized void increment()  {
-        count++;
+    // public synchronized void increment()  {
+    public void increment()  {
+        synchronized(this) {
+            count++;
+        }
     }
 
     public int getCount() {
