c5bfbf67f226faecba00347ebec1194dd64680ec
tanayvaswani <vaswani.tanay9@gmail.com>
2024-08-19 00:57:29 +0530

    feat: Counter class

---
+++ src/com/Counter.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Counter {
+
+    private int count = 0;
+
+    public void increment()  {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
